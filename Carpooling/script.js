15a0b7c2e013af144b3223e98cde43d3c48cdf8c
Esmir <ecaceres@hsconsulting.com.ar>
2024-04-29 07:00:28 -0300

    boton iniciar sesion (nadvar desajustado)

---
+++ Carpooling/script.js
@@ -0,0 +1,11 @@
+
+
+function iniciarSesion(){
+    htmlModal = document.getElementById("modale");
+    htmlModal.setAttribute("class", "modale opened");
+}
+
+function closeModale(){
+    htmlModal = document.getElementById("modale");
+    htmlModal.setAttribute("class", "modale");
+}
